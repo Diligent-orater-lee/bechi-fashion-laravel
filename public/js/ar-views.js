8e3a674592671092a43e8e751e400e03ddfe548b
Diligent orater lee <diligentorater@gmail.com>
2023-08-20 11:44:06 +0530

    ar loaded function implemented

--- public/js/ar-views.js
+++ public/js/ar-views.js
@@ -5,3 +5,17 @@ const ToggleFullscreen = () => {
 const ToggleSound = () => {
     console.log("Toggle Sound");
 }
+
+window.addEventListener('message', function(event) {
+    console.log("Message received: " + event);
+    // Check the origin of the data!
+    if (event.origin !== "http://8thwall.app") { // Replace with the origin of the embedded website
+        console.log("Message received from unknown origin: " + event.origin);
+        return;
+    }
+
+    // Check the message data
+    if (event.data === 'arLoaded') {
+        console.log("AR View loaded");
+    }
+});
